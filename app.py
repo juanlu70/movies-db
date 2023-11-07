eba6eab92fddfd60a9422edbf4313287498a0a46
Juan Luis Martínez <mulder1@gmail.com>
2023-11-07 21:28:23 +0100

    Fixed part of graph generate function

--- app.py
+++ app.py
@@ -1,8 +1,9 @@
 import os
 import sqlite3
 import pandas as pd
-# import matplotlib
-# matplotlib.use('Agg')
+import matplotlib
+import matplotlib.pyplot as plt
+matplotlib.use('Agg')
 
 from flask import Flask, send_file, make_response
 
@@ -35,26 +36,45 @@ def open_db(filename: str):
 def generate_movie_release_chart(movies):
     # Count the number of movies released each year
     df = pd.DataFrame(movies)
+    data = df.groupby(by='release_year').agg({'release_year': 'count'})
 
     # Prepare data for the bar chart
+    data.rename(columns={'release_year': 'movies'}, inplace=True)
+    data.reset_index(drop=True)
+    data['year'] = data.index
+    print("DATA:")
+    print(data)
 
     # Generate the bar chart
-    
+    plt.figure(figsize=(16, 12), dpi=80)
+    plt.title("Movies by year")
+    plt.xlabel("Years")
+    plt.ylabel("Number of movies")
+
     # Customize the chart
+    plt.xticks(data['movies'])
+    plt.plot(data['year'], data['movies'], linewidth=2)
+    plt.grid(True)
 
     # Optionally, rotate x-axis labels if needed
-    plt.xticks(rotation=45)
+    # plt.xticks(rotation=45)
 
     # Save the chart to a file instead of showing it
     plt.savefig('chart.png')
     plt.close()
 
+    return
+
 
 @app.route('/draw-chart')
 def graph_endpoint():
+    sql, bbdd = open_db("database.db")
+    query = "SELECT * FROM movies;"
+    sql.execute(query)
+    movies = sql.fetchall()
     generate_movie_release_chart(movies)
 
-    # Return the generated chart as a file download
+
     return send_file('chart.png', mimetype='image/png')
 
 
@@ -77,10 +97,10 @@ def load_data_endpoint():
             parts = item['title'].split(" (")
             if len(parts) == 2:
                 line['title'] = parts[0]
-                line['release_year'] = parts[1][:-1]
+                line['release_year'] = parts[1][:-1].replace(")", "")
             if len(parts) == 3:
                 line['title'] = parts[0]+" ("+parts[1]
-                line['release_year'] = parts[2][:-1]
+                line['release_year'] = parts[2][:-1].replace(")", "")
             print(line)
             movies.append(line)
 
